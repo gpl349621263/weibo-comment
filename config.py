5dcfaa54902752a66c154cac6c63459ec53a1942
gpl349621263 <49433610+gpl349621263@users.noreply.github.com>
2019-06-06 21:02:44 +0800

    Create config.py

---
+++ config.py
@@ -0,0 +1,38 @@
+#coding:utf-8
+headers = { 
+    'User-agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36', 
+    'Host' : 'm.weibo.cn', 
+    'Accept' : 'application/json, text/plain, */*', 
+    'Accept-Language' : 'zh-CN,zh;q=0.9', 
+    'Accept-Encoding' : 'gzip, deflate, br', 
+    'DNT' : '1', 
+    'Connection' : 'keep-alive', 
+    'Referer':'https://m.weibo.cn/sw.js',
+    'Service-worker': 'script',
+    'Cache-control': 'max-age=0',
+    'Cookie': r'_T_WM=33267025099; ALF=1561191329; SUBP=0033WrSXqPxfM725Ws9jqgMF55529P9D9WFhlxRQSoTeZXxam6Gp2.g_5JpX5K-hUgL.Fo-f1KeNS0.XSo.2dJLoI7UDdc9ydcRX; SCF=AqllgSsy_ttYtI88vrqPHsd-R-AMgcstyH9wlgHtoHYtiNasNH6-lGt2wfRJlg3G3Az4bd0ZXiZHhcaQyNOFUp0.; SUB=_2A25x7j0FDeRhGeNL4lEW9yfIzTWIHXVTEUNNrDV6PUJbkdBeLUnBkW1NSNP9Q5XagLMmHbvzZ40aKh-fvExO43NC; SUHB=0N2FoEEP11lT3l; WEIBOCN_FROM=1110106030; MLOGIN=1; XSRF-TOKEN=d5882c; M_WEIBOCN_PARAMS=oid%3D3786246407205806%26luicode%3D20000174%26lfid%3D3786246407205806'
+    } 
+
+headers_formobile = { 
+    'User-agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36', 
+    'Accept' : 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8', 
+    'Accept-Language' : 'zh-CN,zh;q=0.9', 
+    'Accept-Encoding' : 'gzip, deflate, br', 
+    'Connection' : 'keep-alive', 
+    'Service-worker': 'script',
+    'Content-Type': 'application/x-www-form-urlencoded',
+    'Cache-control': 'max-age=0',
+    'Referer':'https://passport.weibo.cn/signin/login?entry=mweibo'
+    } 
+
+headers_forweb = {
+    'User-agent' : 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36', 
+    'Accept' : 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8', 
+    'Accept-Language' : 'zh-CN,zh;q=0.9', 
+    'Accept-Encoding' : 'gzip, deflate, br', 
+    'Connection' : 'keep-alive', 
+    'Service-worker': 'script',
+    'Content-Type': 'application/x-www-form-urlencoded',
+    'Cache-control': 'max-age=0',
+}
+
